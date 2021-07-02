4a4059ed7945035f921a4adc813617ce0d47668e
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-02 07:43:57 +0200

    add strings

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, ninjas!")
+	// strings
+	var nameOne string = "Mario"
+	var nameTwo = "Luigi"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+	nameOne = "Peach"
+	nameThree = "Bowser"
+	fmt.Println(nameOne, nameTwo, nameThree)
+	nameFour := "Yoshi"
+	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
 }
