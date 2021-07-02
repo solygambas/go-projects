3669559b49f989bac0d7ad117a70aed61747e99f
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-02 07:59:08 +0200

    add integers and floats

--- main.go
+++ main.go
@@ -4,13 +4,34 @@ import "fmt"
 
 func main() {
 	// strings
-	var nameOne string = "Mario"
-	var nameTwo = "Luigi"
-	var nameThree string
-	fmt.Println(nameOne, nameTwo, nameThree)
-	nameOne = "Peach"
-	nameThree = "Bowser"
-	fmt.Println(nameOne, nameTwo, nameThree)
-	nameFour := "Yoshi"
-	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+	// var nameOne string = "Mario"
+	// var nameTwo = "Luigi"
+	// var nameThree string
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameOne = "Peach"
+	// nameThree = "Bowser"
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameFour := "Yoshi"
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	// integers 
+	var ageOne int = 20
+	var ageTwo = 25
+	ageThree := 30
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// https://golang.org/ref/spec#Numeric_types
+	// var numOne int8 = 127
+	// var numTwo int8 = -128
+	// var numThree uint8 = 255
+	// var numFour uint16 = 256
+
+	// floats
+	// var scoreOne float32 = 25.98
+	// var scoreTwo float64 = 25.55555555555555555555555555
+	// scoreThree := 25.98
+
+
+
 }
