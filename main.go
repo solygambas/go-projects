af87414c69fa74af83d79dc4a6c6859ea7429551
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-03 08:22:12 +0200

    add arrays and slices

--- main.go
+++ main.go
@@ -39,8 +39,8 @@ func main() {
 
 	// Println
 	// fmt.Println("new line")
-	age := 35
-	name := "Shaun"
+	// age := 35
+	// name := "Shaun"
 	// fmt.Println("my age is", age, "and my name is", name)
 
 	// Printf (formatted strings)
@@ -53,6 +53,29 @@ func main() {
 	// fmt.Printf("you scored %0.1f points! \n", 225.55) // float 225.6
 
 	// Sprintf (save formatted strings)
-	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-	fmt.Println("the saved string is:", str)
+	// var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	// fmt.Println("the saved string is:", str)
+
+	// arrays (length is fixed)
+	// var ages [3]int = [3]int{20, 25, 30}
+	// var ages = [3]int{20, 25, 30}
+	// fmt.Println(ages, len(ages))
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+	// names[1] = "luigi"
+	// fmt.Println(names, len(names))
+
+	// slices (use arrays under the hood)
+	// var scores = []int{100, 50, 60}
+	// scores[2] = 25
+	// scores = append(scores, 85)
+	// fmt.Println(scores, len(scores))
+
+	// slice ranges
+	// rangeOne := names[1:3] // mario peach
+	// rangeTwo := names[2:] // peach bowser
+	// rangeThree := names[:3] // yoshi mario peach
+	// fmt.Println(rangeOne, rangeTwo, rangeThree)
+	// fmt.Printf("the type of rangeOne is %T \n", rangeOne) // []string
+	// rangeOne = append(rangeOne, "koopa")
+	// fmt.Println(rangeOne) // mario peach koopa
 }
