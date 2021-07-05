2a5638906c1e8308298757bd0f95d90a74ec03f4
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-05 07:57:05 +0200

    add booleans

--- main.go
+++ main.go
@@ -122,4 +122,33 @@ func main() {
 	// for _, value := range names {
 	// 	fmt.Printf("the value is %v \n", value)
 	// }
+
+	// booleans
+	// age := 45
+	// fmt.Println(age <= 50)
+	// fmt.Println(age >= 50)
+	// fmt.Println(age == 45)
+	// fmt.Println(age != 50)
+
+	// if age < 30 {
+	// 	fmt.Println("age is less than 30")
+	// } else if age < 40 {
+	// 	fmt.Println("age is less than 40")
+	// } else {
+	// 	fmt.Println("age is not less than 40")
+	// }
+
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue // skip code after
+		}
+		if index == 2 {
+			fmt.Println("breaking at pos", index)
+			break // stop the loop
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 }
