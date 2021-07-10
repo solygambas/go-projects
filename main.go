fcc2a461a3c27c9538c05df6973e8db000b98f0f
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-10 12:26:32 +0200

    add receiver functions

--- main.go
+++ main.go
@@ -282,5 +282,12 @@ func main() {
 
 	// structs
 	mybill := newBill("mario's bill")
-	fmt.Println(mybill)
+	
+	// receiver functions
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg pie", 8.95)
+	mybill.addItem("toffee pudding", 4.95)
+	mybill.addItem("coffee", 3.25)
+	mybill.updateTip(10)
+	fmt.Println(mybill.format())
 }
