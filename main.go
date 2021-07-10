65103aa107a3811d3b3fb41eaee508589e5fd966
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-10 07:47:17 +0200

    add maps

--- main.go
+++ main.go
@@ -209,11 +209,37 @@ func main() {
 	// fmt.Println(firstName3, secondName3)
 
 	// package scope
-	sayHello("Mario")
+	// sayHello("Mario")
+	// for _, v := range points {
+	// 	fmt.Println(v)
+	// }
+	// showScore()
 
-	for _, v := range points {
-		fmt.Println(v)
+	// maps
+	// menu := map[string]float64{
+	// 	"soup": 4.99,
+	// 	"pie": 7.99,
+	// 	"salad": 6.99,
+	// 	"toffee pudding": 3.55,
+	// }
+	// fmt.Println(menu)
+	// fmt.Println(menu["pie"])
+
+	// looping maps
+	// for k, v := range menu {
+	// 	fmt.Println(k, "-", v)
+	// }
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		984759373: "luigi",
+		845775485: "peach",
 	}
-	
-	showScore()
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[267584967])
+	phonebook[267584967] = "bowser"
+	fmt.Println(phonebook)
+	phonebook[845775485] = "yoshi"
+	fmt.Println(phonebook)
 }
