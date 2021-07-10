3a5b9154cfe206536b85ae37e483716d83f3ef4b
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-10 13:55:32 +0200

    add readme

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Go Projects
 
+## Billing CLI
+
+A bill calculator using the command-line interface to understand GO basics.
+
+<p align="center">
+        <img src="screenshot.png">
+</p>
+
+### Features
+
+- learning about types: strings, integers, floats and maps.
+- printing and formatting strings.
+- defining arrays and slices.
+- importing packages from the standard library.
+- using loops, booleans and conditionals.
+- creating functions and returning multiple values.
+- understanding non-pointer values and pointer wrapper values.
+- making custom data types with structs and interfaces.
+- structuring our code with structs, receiver functions and pointers.
+- handling user input and errors with switch statements.
+- saving the formatted bill to a text file.
+
 Based on [Go Tutorial (Golang) for Beginners](https://www.youtube.com/watch?v=etSN4X_fCnM&list=PL4cUxeGkcC9gC88BEo9czgyS72A3doDeM) by Shaun Pelling - The Net Ninja (2021).
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -52,6 +55,91 @@ func updateMenu(y map[string]float64) {
 
 func updateNameP(x *string) {
 	*x = "wedge"
+}
+
+// interfaces
+type shape interface {
+	area() float64
+	circumf() float64
+}
+
+type square struct {
+	length float64
+}
+type circle struct {
+	radius float64
+}
+
+// square methods
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+// circle methods
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("area of %T is: %0.2f \n", s, s.area())
+	fmt.Printf("circumference of %T is: %0.2f \n", s, s.circumf())
+}
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+		fmt.Println("item added - ", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+		fmt.Println("tip added - ", tip)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("you saved the file - ", b.name)
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
 }
 
 func main() {
@@ -281,13 +369,30 @@ func main() {
 	// fmt.Println(name) // wedge
 
 	// structs
-	mybill := newBill("mario's bill")
+	// mybill := newBill("mario's bill")
 	
 	// receiver functions
-	mybill.addItem("onion soup", 4.50)
-	mybill.addItem("veg pie", 8.95)
-	mybill.addItem("toffee pudding", 4.95)
-	mybill.addItem("coffee", 3.25)
-	mybill.updateTip(10)
-	fmt.Println(mybill.format())
+	// mybill.addItem("onion soup", 4.50)
+	// mybill.addItem("veg pie", 8.95)
+	// mybill.addItem("toffee pudding", 4.95)
+	// mybill.addItem("coffee", 3.25)
+	// mybill.updateTip(10)
+	// fmt.Println(mybill.format())
+
+	// interfaces
+	// shapes := []shape{
+	// 	square{length: 15.2},
+	// 	circle{radius: 7.5},
+	// 	circle{radius: 12.3},
+	// 	square{length: 4.9},
+	// }
+
+	// for _, v := range shapes {
+	// 	printShapeInfo(v)
+	// 	fmt.Println("---")
+	// }
+
+	// user input
+	mybill := createBill()
+	promptOptions(mybill)
 }
